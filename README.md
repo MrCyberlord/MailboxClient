5dee2908b7f790598164a53c42b996b6f67badc9
MrCyberlord <amanjain2691@gmail.com>
2023-06-08 14:53:37 +0530

    First Draft

---
+++ README.md
@@ -0,0 +1,12 @@
+# MailBox Client 
+This is a mailbox project made using using react for a sharpener.tech assignment
+
+[demo link](https://mailbox-sharpener.vercel.app/)
+
+## Features:
+- Login and logout
+- user specific inbox
+- can send, receive and delete mails
+- separate inbox for sent and received mails
+- shows number of read and unread mails
+
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AuthForm from "./components/Auth/AuthForm";
+import Logout from "./components/Auth/Logout";
+import ComposeEmail from "./components/mailbox/composeEmail";
+import Button from "react-bootstrap/Button";
+import Inbox from "./components/mailbox/inbox";
+import MailDeets from "./components/mailbox/mailDeets";
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <AuthForm />
+          </Route>
+          <Route exact path="/welcome">
+            <div style={{ padding: "1rem" }}>
+              <Logout />
+              <a href="/compose">
+                {" "}
+                <Button>Compose email</Button>
+              </a>
+              <div style={{ padding: "1rem" }}>
+                <Inbox />
+              </div>
+            </div>
+          </Route>
+          <Route exact path="/mail/:mailId">
+            <MailDeets />
+          </Route>
+
+          <Route path="/compose">
+            <ComposeEmail />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
